e5524859e35c9fc54c0feaebd75a271e4401c639
G Prajasree <prajasree230@gmail.com>
2025-11-08 21:57:25 +0530

    feat(employee-presence-check): add EmployeePresence class and program logic

---
+++ EmployeeWageApp/UseCases/EmployeePresence.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EmployeeWageApp.UseCases
+{
+    public class EmployeePresence
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Employee Presence Check:");
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2); // 0 = Absent, 1 = Present
+
+            if (empCheck == 1)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
