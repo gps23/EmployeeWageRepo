d46c05e61f4fe6bc3217365acd2b78901b7bbe2a
G Prajasree <prajasree230@gmail.com>
2025-11-09 10:47:41 +0530

    feat(employee-wage-for-multiple-companies): add wage computation for multiple companies

--- EmployeeWageApp/Program.cs
+++ EmployeeWageApp/Program.cs
@@ -12,7 +12,13 @@ namespace EmployeeWageApp
             Console.WriteLine("1. Employee Presence Check");
             Console.WriteLine("2. Daily Wage Calculation");
             Console.WriteLine("3. Part-Time Wage Calculation");
+            Console.WriteLine("4. Employee Wage for Working Days");
+            Console.WriteLine("5. Employee Wage with Conditional Limit");
+            Console.WriteLine("6. Employee Wage Computation Using Switch Statement");
+
+
             Console.Write("Enter choice: ");
+
 
             // Validate input
             if (!int.TryParse(Console.ReadLine(), out int choice))
@@ -34,6 +40,18 @@ namespace EmployeeWageApp
                 case 3:
                     new PartTimeWage().Execute();
                     break;
+                case 4:
+                    new EmployeeWageForDays().Execute();
+                    break;
+                case 5:
+                    new EmployeeWageWithCondition().Execute();
+                    break;
+                case 6:
+                    new EmployeeWageUsingSwitch().Execute();
+                    break;
+
+
+
 
                 default:
                     Console.WriteLine("Invalid choice! Please select a valid option (1–3).");
